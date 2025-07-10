e12caca5c1257a2c432df08d00cc83800b0849de
Ploutarchos Michaelides <info@ploutarchosm.com>
2025-07-10 15:10:43 +0300

    add exports

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from './lib/services/role.service';
+export * from './lib/services/user.service';
+export * from './lib/services/permission.service';
+export * from './lib/services/role-permission.service';
+
+export * from './lib/interfaces/provider.interface';
+export * from './lib/interfaces/user.interface';
+export * from './lib/interfaces/user-service.interface';
+
+export * from './lib/admin.module';
